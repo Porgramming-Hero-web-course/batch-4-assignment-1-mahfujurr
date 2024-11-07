5c28fed9854ad3960a78cebc5f8182e18dfa8415
MD Mahfujur Rahman <mahfujur797@gmail.com>
2024-11-08 01:23:28 +0600

    problem 8 updated

--- problem-8.ts
+++ problem-8.ts
@@ -2,7 +2,7 @@
     type Person = { name: string, age: number, email: string };
     const person: Person = { name: "Alice", age: 25, email: "alice@example.com" };
 
-    const validateKeys = <T>(obj: T, keys: (keyof T)[]): boolean => {
+    const validateKeys = <T extends object>(obj: T, keys: (keyof T)[]): boolean => {
 
         return keys.every((key) => key in obj);
     };
